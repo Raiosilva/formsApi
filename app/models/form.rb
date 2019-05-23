7c4b7791aa8994ae90c9dccb11cc82fd9fa446c0
Raiosilva <raimundopi025@gmail.com>
2019-05-23 20:38:48 -0300

    improving models and migrations

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,3 +1,9 @@
 class Form < ApplicationRecord
   belongs_to :user
+  has_many :questions, dependet: :destroy
+  has_many :answers, dependet: :destroy
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  validates :title, :description, :user, presence: true
+  # validates :title, uniqueness: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,18 @@
 FactoryBot.define do
   timestamp = loop do
-    date = Faker::Time.date.to_time
-    break date.strftime("%F %T") if data <= Date.today
+     data = Faker::Time.between(DateTime.now - 1, DateTime.now).to_time
+     break data.strftime("%F %T") if data <= Date.today
   end
 
   factory :user do
     uid           { Faker::Lorem.word }
     email         { Faker::Internet.email }
     name          { Faker::Name.name }
-    nickname      { Fake::Lorem.word }
+    nickname      { Fake::Internet.user_name }
+    password      { Faker::Internet.password }
     provider      {  'email' }
-    confirmed_at  timestamp
-    created_at    timestamp
-    updated_at    timestamp
+    confirmed_at  {timestamp}
+    created_at    {timestamp}
+    updated_at    {timestamp}
   end
 end
